6223736c55d1aedc3f81048638ef34f09e114b07
latifis <latifirfansyah@gmail.com>
2021-09-09 21:23:58 +0700

    model
    updel models

--- backend/todolist/base/models.py
+++ backend/todolist/base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    role = models.CharField(max_length=200, null=True, blank=True)
     title = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
